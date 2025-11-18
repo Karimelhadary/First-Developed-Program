0000e87861eda92243d28bc68993d4b04ea8d4f4
Karimelhadary <kareemabdulaziz34@yahoo.com>
2025-11-18 16:06:54 +0100

    ..

--- app.py/routes/splash.py
+++ app.py/routes/splash.py
@@ -1,8 +1,9 @@
-from flask import render_template
-from app import app
+from flask import Blueprint, render_template
 
-@app.route("/")
-@app.route("/splash")
-@app.route("/splash.html")
+splash_bp = Blueprint("splash_bp", __name__)
+
+@splash_bp.route("/")
+@splash_bp.route("/splash")
+@splash_bp.route("/splash.html")
 def splash():
     return render_template("splash.html")
